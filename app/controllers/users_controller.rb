c6a92a60090466d74e3135b3df9857e6f86415fa
Scottysk <kwasnickisw@mail.irsc.edu>
2020-05-06 22:21:01 -0400

    added registration form with some functionality

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+
+	def new
+
+	end
+
+	def create
+		@user = User.new
+		@user.username = params[:username]
+		@user.save
+
+		redirect_to "/login"
+	end
+
+	
+end
