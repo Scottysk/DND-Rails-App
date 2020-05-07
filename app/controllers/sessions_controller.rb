f86284c72ff9e067c0762db999a9ca32fa5ee243
Scottysk <kwasnickisw@mail.irsc.edu>
2020-05-06 22:38:10 -0400

    added basic login with basic errors, mostly placeholder

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,14 @@ class SessionsController < ApplicationController
 
 	def new
 	end
-	
+
+	def create
+		@user = User.find_by(:username => params[:username])
+		if @user
+			redirect_to "/"
+		else
+			redirect_to "/login", :notice => "Invalid username or password."
+	end
+end
+
 end
