73a0c0debabaf0752a83377c907d90656d44cfcc
Scottysk <kwasnickisw@mail.irsc.edu>
2020-05-06 22:58:22 -0400

    added temporary navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,13 @@ class ApplicationController < ActionController::Base
 
 	def home
 	end
+
+
+	def current_user
+		if session[:current_user_id]
+			@current_user = User.find(session[:current_user_id])
+		end
+	end
+
 	
 end
