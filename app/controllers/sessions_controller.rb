5f0689c2843b3664b2cc822cc8311a83e5ebf524
Scottysk <kwasnickisw@mail.irsc.edu>
2020-05-09 19:00:29 -0400

    fixed login page redirecting to /users after successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,26 @@
 class SessionsController < ApplicationController
 
 	def new
+		@user = User.new
+		render :login
 	end
 
 	def create
-		@user = User.find_by(:username => params[:username])
-		if @user
-			login(@user)
+		@user = User.find_by(:username => params[:user][:username])
+		if @user && @user.authenticate(params[:user][:password])
+			session[:user_id] = @user.id
+			#login(@user)
 			redirect_to root_path
 		else
-			redirect_to login_path, :notice => "Invalid username or password."
+			redirect_to root_path, :notice => "Invalid username or password."
 		end
 	end
 
+
 	def destroy
-		reset_seession
+		reset_session
 		redirect_to root_path
 	end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,23 @@
 class UsersController < ApplicationController
 
 	def new # GET /register
-		if session[:current_user_id]
-			redirect_to "/", :notice => "You are already logged in."
-		end
+		@user = User.new
 	end
 
 	def create
-		@user = User.new
-		@user.username = params[:username]
-		@user.save
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			redirect_to login_path
+		else
+			render :new
+		end
+	end
 
-		redirect_to "/login"
+	private
+
+	def user_params
+		params.require(:user).permit(:username, :password)
 	end
 
 
