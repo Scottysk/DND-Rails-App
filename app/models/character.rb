0bab1c22dcec5799b24effe76fcb9b5953f5c17e
Scottysk <kwasnickisw@mail.irsc.edu>
2020-05-11 18:49:11 -0400

    added associations and validations for characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,6 @@
 class Character < ApplicationRecord
+	belongs_to :user
+
+	validates :name, :gender, :race, :profession, presence: true
+	validates :name, length: {minimum: 2}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
 	has_secure_password
+
+	has_many :characters
 	validates :username, presence: true
 	validates :username, uniqueness: true
 end
