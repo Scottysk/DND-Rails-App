fb298bc8dd0d3d0fd23a86326ccea53b9a5941d4
Scottysk <kwasnickisw@mail.irsc.edu>
2020-05-11 18:38:17 -0400

    added temp methods in the characters controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,13 @@
 class CharactersController < ApplicationController
+
+	def new
+	end
+
+	def create
+	end
+
+	def index
+	end
+
+	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_012137) do
+ActiveRecord::Schema.define(version: 2020_05_11_223423) do
+
+  create_table "characters", force: :cascade do |t|
+    t.string "name"
+    t.string "gender"
+    t.string "race"
+    t.string "profession"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
