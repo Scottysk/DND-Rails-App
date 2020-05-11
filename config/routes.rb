4498d2e46c5c27fd2062ea51fb490e5782cc9e84
Scottysk <kwasnickisw@mail.irsc.edu>
2020-05-11 19:23:20 -0400

    added show page for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :characters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users
 
   root 'application#home'
 
@@ -10,5 +8,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :users
+  resources :characters
 
 end
