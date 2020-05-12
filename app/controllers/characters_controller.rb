69d735f2ddb454d0dd7a3a8ed932bf3c1edc442b
Scottysk <kwasnickisw@mail.irsc.edu>
2020-05-11 20:41:18 -0400

    added temporary html for new character form, created character methods to make it function

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,13 +1,25 @@
 class CharactersController < ApplicationController
 
 	def new
+		@character = Character.new
 	end
 
 	def create
+		@character = Character.new(character_params)
+		if @character.save
+			redirect_to characters_path
+		else
+			render :new
+		end
 	end
 
 	def index
 	end
+
+	private
+
+	def post_params
+		params.require(:character).permit(:name, :gender, :race, :profession)
 
 	
 end
