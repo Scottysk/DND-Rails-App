78a301e22a0a44cffbb84ee2b1c83eae6eef4f92
Scottysk <kwasnickisw@mail.irsc.edu>
2020-05-16 16:44:03 -0400

    cleaned up homepage somewhat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::Base
 		@current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
 	end
 
+
 	def logged_in?
 		!!session[:user_id]
 	end
