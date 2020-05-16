85b2fcd7c02c03bbe9bb7e547471d7e516bbaf2c
Scottysk <kwasnickisw@mail.irsc.edu>
2020-05-16 18:22:16 -0400

    added comment feed to user's homepage

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,2 +1,15 @@
 module CharactersHelper
+
+	def characters_visibility
+		if @user.characters.empty?
+			tag.h3(link_to('You have no characters yet. Create one here!', new_character_path))
+		else
+
+
+
+		end
+	end
+
+
+
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,16 @@
 module CommentsHelper
+
+def comments_visibility
+	if @user.comments.empty?
+			tag.h3(link_to("You have no comments yet. Visit a character's page to comment on them!"))
+	else
+
+
+
+end
+
+
+
+end
+
 end
