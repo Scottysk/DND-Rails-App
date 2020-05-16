7150ff4ef5327346928561661ba5436adadcab91
Scottysk <kwasnickisw@mail.irsc.edu>
2020-05-16 19:00:18 -0400

    added some test css, fixed more typos

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -5,9 +5,14 @@ module CharactersHelper
 			tag.h3(link_to('You have no characters yet. Create one here!', new_character_path))
 		else
 
-
-
 		end
+	end
+
+	def edit_visibility
+			 @character.user == current_user 
+			(button_to('Edit', edit_character_path))
+
+
 	end
 
 
